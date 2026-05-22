16c570715adb7b1709f98b467d235469b47b49bc
Moon Hyuk Choi <dotnetpower@hotmail.com>
2026-05-22 22:19:54 +0900

    chore(web): F6 ignore .tsbuild in eslint config

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist/**", "node_modules/**", "*.tsbuildinfo"],
+    ignores: ["dist/**", "node_modules/**", "*.tsbuildinfo", ".tsbuild/**"],
   },
   ...tseslint.configs.recommended,
   {
